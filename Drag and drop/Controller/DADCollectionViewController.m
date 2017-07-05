e4bf289ef8dcd0a5ae26d12893cc6e89c1b78280
Martin <crh944015688@gmail.com>
2017-07-06 00:25:58 +0800

    添加collectionView

--- Drag and drop/Controller/DADCollectionViewController.m
+++ Drag and drop/Controller/DADCollectionViewController.m
@@ -8,9 +8,11 @@
 
 #import "DADCollectionViewController.h"
 
-@interface DADCollectionViewController () <UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDragDelegate, UICollectionViewDropDelegate>
+#define CellReuseIdentifier @"CellReuseIdentifier"
+@interface DADCollectionViewController () <UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDragDelegate, UICollectionViewDropDelegate>
 
 @property (nonatomic, strong) UICollectionView *collectionView;
+@property (nonatomic, strong) NSMutableArray *dataArray;
 
 @end
 
@@ -18,16 +20,63 @@
 
 - (id)initWithTitleName:(NSString *)titleName {
     if (self = [super init]) {
-        self.view.backgroundColor = [UIColor whiteColor];
         self.navigationItem.title = titleName;
+        [self loadData];
     }
     return self;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    [self creatCollectionView];
     // collectionView
+}
+
+- (void)loadData {
+    self.dataArray = [[NSMutableArray alloc] initWithObjects:@"pic1", @"pic2", @"pic3", @"pic4", @"pic5", @"pic6", nil];
+}
+
+- (void)creatCollectionView {
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+    layout.scrollDirection = UICollectionViewScrollDirectionVertical;
+    layout.sectionInset = UIEdgeInsetsMake(0, 0, 0, 0);
+    layout.minimumInteritemSpacing = 5;
+    //上下两个item的最小间距
+    layout.minimumLineSpacing = 5;
+    
+    self.collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:layout];
+    [self.view addSubview:self.collectionView];
+    
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
+    self.collectionView.alwaysBounceVertical = YES;
+    self.collectionView.backgroundColor = [UIColor whiteColor];
+    
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:CellReuseIdentifier];
+}
+
+#pragma mark - CollectionViewDelegate
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return self.dataArray.count;
+}
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 1;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:CellReuseIdentifier forIndexPath:indexPath];
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:self.dataArray[indexPath.row]]];
+    imageView.frame = cell.frame;
+    [cell addSubview:imageView];
+    
+    return cell;
+}
+
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return CGSizeMake(300, 150);
 }
 
 @end
